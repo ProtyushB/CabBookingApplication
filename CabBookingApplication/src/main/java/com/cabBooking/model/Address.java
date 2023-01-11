3ed6cbc424531f1945326eb58a452a87e7cfb3ee
Protyush Banerjee <protyushprincebanerjee@gmail.com>
2023-01-11 13:18:28 +0530

    CustomerServiceImpl Update

--- CabBookingApplication/src/main/java/com/cabBooking/model/Address.java
+++ CabBookingApplication/src/main/java/com/cabBooking/model/Address.java
@@ -1,8 +1,16 @@
 package com.cabBooking.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class Address {
 
     @Id
@@ -22,82 +30,4 @@ public class Address {
     String city;
 
     String pincode;
-
-    public Address() {
-    }
-
-    public Address(Long addressId, String apartmentNumber, String buildingName, String streeName, String coutry, String state, String city, String pincode) {
-        this.addressId = addressId;
-        this.apartmentNumber = apartmentNumber;
-        this.buildingName = buildingName;
-        this.streeName = streeName;
-        this.coutry = coutry;
-        this.state = state;
-        this.city = city;
-        this.pincode = pincode;
-    }
-
-    public Long getAddressId() {
-        return addressId;
-    }
-
-    public void setAddressId(Long addressId) {
-        this.addressId = addressId;
-    }
-
-    public String getApartmentNumber() {
-        return apartmentNumber;
-    }
-
-    public void setApartmentNumber(String apartmentNumber) {
-        this.apartmentNumber = apartmentNumber;
-    }
-
-    public String getBuildingName() {
-        return buildingName;
-    }
-
-    public void setBuildingName(String buildingName) {
-        this.buildingName = buildingName;
-    }
-
-    public String getStreeName() {
-        return streeName;
-    }
-
-    public void setStreeName(String streeName) {
-        this.streeName = streeName;
-    }
-
-    public String getCoutry() {
-        return coutry;
-    }
-
-    public void setCoutry(String coutry) {
-        this.coutry = coutry;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getPincode() {
-        return pincode;
-    }
-
-    public void setPincode(String pincode) {
-        this.pincode = pincode;
-    }
 }
--- CabBookingApplication/src/main/java/com/cabBooking/serviceLayer/customer/CustomerService.java
+++ CabBookingApplication/src/main/java/com/cabBooking/serviceLayer/customer/CustomerService.java
@@ -1,7 +1,13 @@
 package com.cabBooking.serviceLayer.customer;
 
+import com.cabBooking.model.Customer;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface CustomerService {
+
+    Customer addCustomer(Customer customer);
+    Customer updateCustomer(Customer customer);
+    Customer viewCustomer(Long customerId);
+    String deleteCustomer(Long customerId);
 }
