bdcfb67b999240ebc2433566d043b131120ec600
Protyush Banerjee <protyushprincebanerjee@gmail.com>
2023-02-05 12:04:00 +0530

    Changes

--- CabBookingApplication/src/main/java/com/cabBooking/model/Booking.java
+++ CabBookingApplication/src/main/java/com/cabBooking/model/Booking.java
@@ -35,7 +35,7 @@ public class Booking {
 
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     Location rideEndingPoint;
-    
+
     String bookingStatus;       // 1.Active     2.Completed     3.Upcoming
 
 
