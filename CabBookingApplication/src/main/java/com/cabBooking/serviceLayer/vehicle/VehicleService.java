62b95e02e920ebb2382d9cd29f3aecac108e32d3
Protyush Banerjee <protyushprincebanerjee@gmail.com>
2023-02-06 13:38:41 +0530

    Methods Updated => VehicleService

--- CabBookingApplication/src/main/java/com/cabBooking/serviceLayer/vehicle/VehicleService.java
+++ CabBookingApplication/src/main/java/com/cabBooking/serviceLayer/vehicle/VehicleService.java
@@ -1,10 +1,23 @@
 package com.cabBooking.serviceLayer.vehicle;
 
+import com.cabBooking.model.Booking;
+import com.cabBooking.model.Driver;
 import com.cabBooking.model.Vehicle;
+
+import java.util.List;
 
 public interface VehicleService {
     Vehicle addVehicle(Vehicle vehicle);
     Vehicle updateVehicle(Vehicle vehicle);
     Vehicle viewVehicle(Long vehicleId);
     String deleteVehicle(Long vehicleId);
+
+    Vehicle assignDriverToVehicle(Long vehicleId, Driver driver);
+    Vehicle removeDriverFromVehicle(Long vehicleId, Long driverId);
+
+    List<Booking> getAllBookings(Long vehicleId);
+    List<Booking> getBookingHistory(Long vehicleId);
+    List<Booking> getActiveBookings(Long vehicleId);
+    List<Booking> getUpComingBookings(Long vehicleId);
+    List<Booking> getCancelledBookings(Long vehicleId);
 }
