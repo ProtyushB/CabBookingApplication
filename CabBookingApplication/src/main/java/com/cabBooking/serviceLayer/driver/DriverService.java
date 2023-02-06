2710300139bc0804a375e32a3faeb088053be09a
Protyush Banerjee <protyushprincebanerjee@gmail.com>
2023-02-06 13:24:24 +0530

    Update Methods => DriverService

--- CabBookingApplication/src/main/java/com/cabBooking/serviceLayer/driver/DriverService.java
+++ CabBookingApplication/src/main/java/com/cabBooking/serviceLayer/driver/DriverService.java
@@ -1,10 +1,21 @@
 package com.cabBooking.serviceLayer.driver;
 
+import com.cabBooking.model.Booking;
 import com.cabBooking.model.Driver;
+
+import java.util.List;
 
 public interface DriverService {
     Driver addDriver(Driver driver);
     Driver updateDriver(Driver driver);
     Driver viewDriver(Long driverId);
     String deleteDriver(Long driverId);
+
+    String acceptBooking(Booking booking);
+    String rejectBooking(Booking booking);
+
+    List<Booking> getAllBookings(Long driverId);
+    List<Booking> getBookingHistory(Long driverId);
+    List<Booking> getActiveBookings(Long driverId);
+    List<Booking> getCencelledBookings(Long driverId);
 }
